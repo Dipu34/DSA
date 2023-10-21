b6114579d56d873a4cce3919d71bb1e7c776bf56
Dipendra Sah <131299802+Dipu34@users.noreply.github.com>
2023-10-21 18:28:09 +0530

    java

---
+++ q3.java
@@ -0,0 +1,19 @@
+
+public class q3 {
+	public static int reverse(int number) {
+		int reversed = 0;
+		while (number != 0) {
+		int remainder= number % 10;
+		reversed = reversed * 10 + remainder ;
+		number /= 10;
+		}
+		return reversed;
+		}
+		public static void main(String[] args) {
+		int number = 12345;
+		int reversed = reverse(number);
+		System.out.println("Reverse of " + number + " is " + reversed);
+		}
+		}
+
+
