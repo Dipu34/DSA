ce87a03d2a17785ec867792c9a5f6e32df49761e
Dipendra Sah <131299802+Dipu34@users.noreply.github.com>
2023-11-01 00:15:11 +0530

    multithreading

--- q1.java
+++ q1.java
@@ -1,27 +1,32 @@
-
+package ass3;
+class A implements Runnable{
+	 public void run()
+	 {
+		 for(int i=0;i<5;i++) {
+			 System.out.println("HELLO");
+			 try {Thread.sleep(100);}catch(Exception e) {}
+			 }
+	 }
+}
+	 class B implements Runnable{
+		 public void run() {
+			 for(int j=0;j<5;j++) {
+				 System.out.println("HI");
+				 try {Thread.sleep(100);}catch(Exception e) {}
+			 }
+		 }
+	 
+	 }
 public class q1 {
 
-	
-		public static boolean isPrime(int num) {
-			if (num <= 1) {
-			return false;
-			}
-			for (int i = 2; i <= Math.sqrt(num); i++) {
-			if (num % i == 0) {
-			return false;
-			}
-			}
-			return true;
-			}
-			public static void main(String[] args) {
-			int num = 17;
-			if (isPrime(num)) {
-			System.out.println(num + " is a prime number");
-			} else {
-			System.out.println(num + " is not a prime number");
-			}
-			 
-			
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 A obj= new A();
+		    Thread t=new Thread(obj);
+		       t.start();
+		       B obj1= new B();
+		       Thread t1=new Thread(obj1);
+		       t1.start();
 	}
 
 }
--- q2.java
+++ q2.java
@@ -1,21 +1,34 @@
 
-public class q2 {
-	public static String decimalToBinary(int decimal) {
-		if (decimal == 0) {
-		return "0";
-		}
-		StringBuilder binary = new StringBuilder();
-		while (decimal > 0) {
-		int rem = decimal % 2;
-		binary.append(rem);
-		decimal /= 2;
-		}
-		return binary.reverse().toString();
-		}
-		public static void main(String[] args) {
-		int decimal = 15;
-		String binary = decimalToBinary(decimal);
-		System.out.println("Binary equivalent of " + decimal + " is " +
-		binary);
+package ass3;
+import java.util.*;
+class workerThread extends Thread{
+	int x,y;
+	workerThread(int n1,int n2){
+		x=n1;
+		y=n2;
+		
+	}
+	
+
+public void run() {
+	int result = x+y;
+	onResult(result);
 }
+void onResult(int result) {
+	System.out.println("recevied result from the thread:"+result);
+}
+}
+public class q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc=new Scanner(System.in);
+	     int a,b;
+	     System.out.println("enter two number");
+	     a=sc.nextInt();
+	     b=sc.nextInt();
+	     workerThread workerThread= new workerThread(a,b);
+	     workerThread.start();
+	}
+
 }
